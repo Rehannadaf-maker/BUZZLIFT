8f1c33c729e3b6d5193cd7de73eaa3f15104c815
Rehan Nadaf <suhananadaf35@gmail.com>
2025-09-03 20:16:08 +0530

    Cleaned up gitignore and remove cached files

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import Order from "../models/Order.js";
+import Order from "../models/order.js";
 
 const router = express.Router();
 
